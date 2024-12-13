47ad91c5e385bca1405f8476f756063b486d3569
Numi <mvalenzuelarobledo@safareyes.es>
2024-12-13 10:20:02 +0100

    Git Frontend

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '2DAM-DI-AD-P1-Frontend',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.scss'],
+  standalone: true,
+})
+export class PrincipalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
