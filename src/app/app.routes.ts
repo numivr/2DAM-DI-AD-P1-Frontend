b6abc989c5b46e168551dd526056f3e6865b2dc0
Numi <mvalenzuelarobledo@safareyes.es>
2025-01-15 10:48:22 +0100

    estructura de pantallas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import {InicioComponent} from "./inicio/inicio.component";
-import {RegistroComponent} from "./registro/registro.component";
+import {ChatMensajesComponent} from "./chat-mensajes/chat-mensajes.component";
+import {ChatsPersonasComponent} from "./chats-personas/chats-personas.component";
+import {RegistroCualidadesComponent} from "./registro-cualidades/registro-cualidades.component";
+import {RegistroPerroComponent} from "./registro-perro/registro-perro.component";
+import {RegistroCuentaComponent} from "./registro-cuenta/registro-cuenta.component";
+import {PublicacionComponent} from "./publicacion/publicacion.component";
 import {PrincipalComponent} from "./principal/principal.component";
 import {PerfilComponent} from "./perfil/perfil.component";
-import {PerfilPropioComponent} from "./perfil-propio/perfil-propio.component";
-import {ChatComponent} from "./chat/chat.component";
 
 export const routes: Routes = [
   {
@@ -14,30 +17,41 @@ export const routes: Routes = [
 
 
   {
-    path: 'inicio',
+    path: 'Inicio',
     component: InicioComponent,
   },
   {
-    path: 'registro',
-    component: RegistroComponent,
+    path: 'ChatMensajes',
+    component: ChatMensajesComponent,
   },
   {
-    path: 'principal/:idUsuario',
+    path: 'ChatsPersonas',
+    component: ChatsPersonasComponent,
+  },
+  {
+    path: 'RegistroCualidades',
+    component: RegistroCualidadesComponent,
+  },
+  {
+    path: 'RegistroPerro',
+    component: RegistroPerroComponent,
+  },
+  {
+    path: 'RegistroCuenta',
+    component: RegistroCuentaComponent,
+  },
+  {
+    path: 'Publicacion',
+    component: PublicacionComponent,
+  },
+  {
+    path: 'Principal',
     component: PrincipalComponent,
   },
   {
-    path: 'perfil',
+    path: 'Perfil',
     component: PerfilComponent,
   },
-  {
-    path: 'perfil_propio/:idUsuario',
-    component: PerfilPropioComponent,
-  },
-  {
-    path: 'chat/:idUsuario',
-    component: ChatComponent,
-  },
-
 
   {
     path: '',
