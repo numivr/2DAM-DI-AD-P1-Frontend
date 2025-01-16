3eec8d4dd607bca5da8c9e00c7900b3cdfae4b46
Numi <mvalenzuelarobledo@safareyes.es>
2025-01-16 14:18:29 +0100

    primera parte de inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.scss'],
   standalone: true,
+  imports: [
+    IonicModule
+  ]
 })
 export class InicioComponent  implements OnInit {
 
