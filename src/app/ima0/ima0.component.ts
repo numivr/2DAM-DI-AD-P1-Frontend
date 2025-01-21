4422f4c72ac1a5ddfce05e764093855dd6dd6de2
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-01-21 13:25:35 +0100

    Creado imagen y empezado Perfil

---
+++ src/app/ima0/ima0.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ima0',
+  templateUrl: './ima0.component.html',
+  styleUrls: ['./ima0.component.scss'],
+})
+export class Ima0Component
+{
+  constructor() { }
+
+}
--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {IonContent} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrls: ['./perfil.component.scss'],
   standalone: true,
+  imports: [
+    IonContent
+  ]
 })
 export class PerfilComponent  implements OnInit {
 
