8d10fe07cfdc7ee20e75e9ce86a7722014ed853c
Numi <mvalenzuelarobledo@safareyes.es>
2025-01-23 12:20:25 +0100

    problemitas con el css alert

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,37 +1,56 @@
 import {Component, OnInit} from '@angular/core';
-import {IonicModule} from "@ionic/angular";
+import {AlertController, IonAlert, IonicModule} from "@ionic/angular";
 import {NgOptimizedImage} from "@angular/common";
 import {add, chatbubblesOutline, personCircle} from "ionicons/icons";
 import {addIcons} from "ionicons";
 import {Usuario} from "../models/Usuario";
 import {Publicacion} from "../models/Publicacion";
 import {ComponentePublicacionComponent} from "../componentes/componente-publicacion/componente-publicacion.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
-    selector: 'app-principal',
-    templateUrl: './principal.component.html',
-    styleUrls: ['./principal.component.scss'],
-    standalone: true,
-    imports: [
-        IonicModule,
-        NgOptimizedImage,
-        ComponentePublicacionComponent
-    ]
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    NgOptimizedImage,
+    ComponentePublicacionComponent,
+    RouterLink
+  ]
 })
 export class PrincipalComponent implements OnInit {
-    constructor() {
-    }
+  constructor(private alertController: AlertController) {
+  }
 
-    usuarios: Usuario[] = [];
-    publicaciones: Publicacion[] = [];
+  usuarios: Usuario[] = [];
+  publicaciones: Publicacion[] = [];
 
+  public alertButtons = ['OK'];
+  public alertInputs = [
+    {
+      type: 'textarea' as const,
+      placeholder: 'Escribe tu publicación',
+    },
+  ];
 
-    ngOnInit() {
-        addIcons({
-            'add': add,
-            'chatbubbles-outline': chatbubblesOutline,
-            'person-circle': personCircle,
-        });
-    }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¡Cuéntanos!',
+      buttons: this.alertButtons,
+      inputs: this.alertInputs,
+      cssClass: 'custom-alert' // Clase CSS aplicada
+    });
 
+    await alert.present();
+  }
+
+  ngOnInit() {
+    addIcons({
+      'add': add,
+      'chatbubbles-outline': chatbubblesOutline,
+      'person-circle': personCircle,
+    });
+  }
 }
