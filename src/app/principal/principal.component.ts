306df3a2403e572995d57e3aab9856d1be11ca05
Numi <mvalenzuelarobledo@safareyes.es>
2025-01-28 20:04:26 +0100

    searchbar final

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
-import { IonModal, IonicModule } from '@ionic/angular';
-import { add, chatbubblesOutline, personCircle } from 'ionicons/icons';
-import { addIcons } from 'ionicons';
-import { Usuario } from '../models/Usuario';
-import { Publicacion } from '../models/Publicacion';
-import { ComponentePublicacionComponent } from '../componentes/componente-publicacion/componente-publicacion.component';
-import { RouterLink } from '@angular/router';
-import { NgIf, NgOptimizedImage } from '@angular/common';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonModal, IonicModule} from '@ionic/angular';
+import {add, chatbubblesOutline, personCircle} from 'ionicons/icons';
+import {addIcons} from 'ionicons';
+import {Usuario} from '../models/Usuario';
+import {Publicacion} from '../models/Publicacion';
+import {ComponentePublicacionComponent} from '../componentes/componente-publicacion/componente-publicacion.component';
+import {RouterLink} from '@angular/router';
+import {NgIf, NgOptimizedImage} from '@angular/common';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-principal',
@@ -18,16 +19,15 @@ import { NgIf, NgOptimizedImage } from '@angular/common';
     NgOptimizedImage,
     ComponentePublicacionComponent,
     RouterLink,
-    NgIf
+    NgIf,
+    FormsModule
   ],
 })
 export class PrincipalComponent implements OnInit {
-  @ViewChild('modal', { static: true }) modal!: ElementRef;
   selectedSegment: string = 'Recomendado';
   usuarios: Usuario[] = [];
   publicaciones: Publicacion[] = [];
-
-  constructor(private renderer: Renderer2) {}
+  isModalOpen: boolean = false;
 
   ngOnInit() {
     addIcons({
@@ -35,24 +35,38 @@ export class PrincipalComponent implements OnInit {
       'chatbubbles-outline': chatbubblesOutline,
       'person-circle': personCircle,
     });
-  }
 
-  segmentChanged(event: any) {
-    this.selectedSegment = event.detail.value;
+    // Ensure the modal is closed initially
+    const modal = document.querySelector('ion-modal');
+    if (modal) {
+      modal.classList.add('closed');
+    }
   }
-
-  isModalOpen = false; // Estado para controlar la visibilidad del modal
 
   openModal() {
-    this.renderer.setStyle(this.modal.nativeElement, 'visibility', 'visible');
-    this.renderer.setStyle(this.modal.nativeElement, 'height', '50%');
-    this.isModalOpen = true; // Abre el modal
+    this.isModalOpen = true;
+    const modal = document.querySelector('ion-modal');
+    if (modal) {
+      modal.classList.remove('closed');
+      modal.classList.add('open');
+    }
   }
 
   closeModal() {
-    this.isModalOpen = false; // Cierra el modal
-    this.renderer.setStyle(this.modal.nativeElement, 'visibility', 'hidden');
-    this.renderer.setStyle(this.modal.nativeElement, 'height', '0');
+    const modal = document.querySelector('ion-modal');
+    if (modal) {
+      modal.classList.remove('open');
+      modal.classList.add('closed');
+    }
+    // Usa un delay para sincronizar el cambio de estado con la animación
+    setTimeout(() => {
+      this.isModalOpen = false;
+    }, 300);
+  }
+
+
+  segmentChanged(event: any) {
+    this.selectedSegment = event.detail.value;
   }
 
   truncateText(text: string, limit: number = 40): string {
