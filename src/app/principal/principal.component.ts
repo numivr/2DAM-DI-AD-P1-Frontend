2b55662568088943284a6e447b0fd94767100169
Numi <mvalenzuelarobledo@safareyes.es>
2025-01-28 12:57:34 +0100

    pa vilches

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { IonModal, IonicModule } from '@ionic/angular';
 import { add, chatbubblesOutline, personCircle } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
@@ -22,10 +22,12 @@ import { NgIf, NgOptimizedImage } from '@angular/common';
   ],
 })
 export class PrincipalComponent implements OnInit {
+  @ViewChild('modal', { static: true }) modal!: ElementRef;
   selectedSegment: string = 'Recomendado';
   usuarios: Usuario[] = [];
   publicaciones: Publicacion[] = [];
 
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     addIcons({
@@ -42,15 +44,18 @@ export class PrincipalComponent implements OnInit {
   isModalOpen = false; // Estado para controlar la visibilidad del modal
 
   openModal() {
+    this.renderer.setStyle(this.modal.nativeElement, 'visibility', 'visible');
+    this.renderer.setStyle(this.modal.nativeElement, 'height', '50%');
     this.isModalOpen = true; // Abre el modal
   }
 
   closeModal() {
     this.isModalOpen = false; // Cierra el modal
+    this.renderer.setStyle(this.modal.nativeElement, 'visibility', 'hidden');
+    this.renderer.setStyle(this.modal.nativeElement, 'height', '0');
   }
 
   truncateText(text: string, limit: number = 40): string {
     return text.length > limit ? `${text.substring(0, limit)}...` : text;
   }
-
 }
