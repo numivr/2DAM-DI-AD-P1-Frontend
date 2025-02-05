1be5fc84449e94bf7429593dbf0b7c651af8dc00
Sui <149364828+PabloGHub@users.noreply.github.com>
2025-02-05 13:47:32 +0100

    publicacion principio implementacion likes

--- src/app/publicacion/publicacion.component.ts
+++ src/app/publicacion/publicacion.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {ComponentePublicacionComponent} from "../componentes/componente-publicacion/componente-publicacion.component";
-import {NgIf} from "@angular/common";
-import {IonContent} from "@ionic/angular/standalone";
+import { ComponentePublicacionComponent } from '../componentes/componente-publicacion/componente-publicacion.component';
+import { NgIf } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-publicacion',
   templateUrl: './publicacion.component.html',
   styleUrls: ['./publicacion.component.scss'],
   standalone: true,
-    imports: [
-        IonContent,
-        ComponentePublicacionComponent
-    ]
+  imports: [
+    IonicModule,
+    ComponentePublicacionComponent,
+    NgIf
+  ]
 })
 export class PublicacionComponent  implements OnInit
 {
   constructor() { }
   ngOnInit() {}
 
+  async toggleFavorite() {
+    this.isFavorite = !this.isFavorite; // Alterna entre true y false
+    console.log("Estado cambiado a:", this.isFavorite); // Debug
+  }
+
   m_nombre_s: string = '@Lucas';
   m_texto_s: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dignissim rhoncus mi in vulputate. Sed suscipit, diam vel tincidunt consequat, tortor aliquet turpis, sed elementum nibh mauris sit amet elit. ';
   m_image_s: string = "";
+
+  isFavorite: boolean = false;
+  likes: number|null = null;
+  comentarios: number|null = null;
 }
