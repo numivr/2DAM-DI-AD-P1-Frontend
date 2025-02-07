d99354f296a717af550e32e570c65972617eaf3a
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2025-02-07 17:43:39 +0100

    publicacion componente areglado

--- src/app/componentes/componente-publicacion/componente-publicacion.component.ts
+++ src/app/componentes/componente-publicacion/componente-publicacion.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { IonicModule } from "@ionic/angular";
-import { addIcons } from "ionicons";
-import { chatbubbleOutline, heart, heartOutline } from "ionicons/icons";
-import { NgIf } from "@angular/common";
-import { RouterLink } from "@angular/router";
+import {Component, Input, numberAttribute, OnInit} from '@angular/core';
+import {IonicModule} from "@ionic/angular";
+import {addIcons} from "ionicons";
+import {chatbubbleOutline, heart, heartOutline} from "ionicons/icons";
+import {NgIf} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-componente-publicacion',
@@ -16,28 +16,38 @@ import { RouterLink } from "@angular/router";
     RouterLink,
   ]
 })
-/* No tocar nada de los demas */
-export class ComponentePublicacionComponent implements OnInit {
-  @Input() perfil: string | null = null;  // Nombre del creador
-  @Input() fotoPerfil: string | null = null;  // URL de la foto de perfil
-  @Input() texto: string = 'Texto de ejemplo';
-  @Input() fotoPublicacion: string | null = null; // URL de la imagen de la publicación
-  @Input() numLikes: number | null = 0;
-  @Input() numComentarios: number | null = 0;
-  @Input() liked: boolean = false;
+export class ComponentePublicacionComponent  implements OnInit
+{
+  @Input() enlaceUsuario: string = '#'; // Enlace al perfil del usuario
+  @Input() enlace: string = '#'; // Enlace a la publicación
+  @Input() isFavorite: boolean = false; // Indica si la publicación es favorita
+  @Input() urlImagen: string|null = null; // URL de la imagen
+  m_miNombre_s: string|null = ""; // Nombre del usuario, Porcesado
+  @Input() nombre: string|null = null; // Nombre del usuario
+  @Input() texto: string = 'texto_ejemplo';  // Texto de la publicación
+  @Input({transform: numberAttribute}) likes: number|null = null; // Número de likes
+  @Input({transform: numberAttribute}) comentarios: number|null = null; // Número de comentarios
+
+  @Input() miUrl: string|null = null;
 
   constructor() { }
 
   async toggleFavorite() {
-    this.liked = !this.liked; // Alterna entre true y false
-    this.numLikes = this.liked ? (this.numLikes || 0) + 1 : (this.numLikes || 1) - 1;
+    this.isFavorite = !this.isFavorite; // Alterna entre true y false
+    console.log("Estado cambiado a:", this.isFavorite); // Debug
   }
+
 
   ngOnInit() {
     addIcons({
       'heart-outline': heartOutline,
       'chatbubble-outline': chatbubbleOutline,
       'heart': heart,
-    });
+    })
+
+    this.m_miNombre_s = '@' + (this.nombre !== null ? this.nombre : null);
+
+    // console.log(this.urlImagen);
   }
+
 }
