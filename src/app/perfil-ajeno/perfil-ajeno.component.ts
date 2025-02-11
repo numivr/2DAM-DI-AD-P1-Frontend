b1dbcfdffed48c273ab5a92fc9824e070904fa87
Rodrigo-Toledo-ALt <29533255J@safareyes.es>
2025-02-11 12:15:42 +0100

    perfil ajeno funciona, sin botón para redirigir, falta hacer el router link, pa numi

--- src/app/perfil-ajeno/perfil-ajeno.component.ts
+++ src/app/perfil-ajeno/perfil-ajeno.component.ts
@@ -22,6 +22,7 @@ import { UsuarioService } from "../1-Servicios/usuario.service"; // ✅ Importam
     NgOptimizedImage,
     RouterLink,
     NgForOf
+
   ]
 })
 export class PerfilAjenoComponent implements OnInit {
