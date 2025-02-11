606591810137bbfcb2da1570e0707352636af352
Numi <mvalenzuelarobledo@safareyes.es>
2025-02-11 13:23:25 +0100

    reset

---
+++ src/app/app/componente-comentario/componente-comentario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-comentario',
+  templateUrl: './componente-comentario.component.html',
+  styleUrls: ['./componente-comentario.component.scss'],
+  standalone: true,
+})
+export class ComponenteComentarioComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
