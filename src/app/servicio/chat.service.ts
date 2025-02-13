9c5fc0c71e48b558dd665a1fbad13c0b40828d02
Vilches <victormvg21@gmail.com>
2025-02-13 12:45:04 +0100

    Insertado mensajes, con horrible estilo

--- src/app/servicio/chat.service.ts
+++ src/app/servicio/chat.service.ts
@@ -1,37 +1,34 @@
 import { Injectable } from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Chat} from "../models/Chat";
-import {BehaviorSubject, Observable} from "rxjs";
-import {environment} from "../../environments/environment";
-import {Mensaje} from "../models/Mensaje";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Chat } from "../models/Chat";
+import { BehaviorSubject, Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { Mensaje } from "../models/Mensaje";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-  private urlChats  = '/mensaje/chats';
-  private urlMensajeEnviar  = '/mensaje/enviar';
-  private urlMensajesChatPrivado  = '/mensaje/chat/';
+  private urlChats = '/mensaje/chats';
+  private urlMensajeEnviar = '/mensaje/enviar';
+  private urlMensajesChatPrivado = '/mensajes/conversacion/';
   private apiUrl = environment.apiUrl;
-  private contactoObservable = new BehaviorSubject<number>(0);
-  contacto = this.contactoObservable.asObservable();
+  chatObservable = new BehaviorSubject<number>(0);
 
 
   private usuarioObservable = new BehaviorSubject<number>(0);
   usuario = this.usuarioObservable.asObservable();
 
+
   constructor(private http: HttpClient) { }
 
   private getHeaders(): HttpHeaders {
     const token = sessionStorage.getItem('auth-token');
-
     if (!token) {
       console.error('❌ No hay token en sessionStorage. Asegúrate de que el usuario haya iniciado sesión.');
-      return new HttpHeaders(); // Devolvemos headers vacíos para evitar errores
+      return new HttpHeaders();
     }
-
     console.log('✅ Token enviado en el header:', token);
-
     return new HttpHeaders({
       Authorization: `Bearer ${token}`,
       'Content-Type': 'application/json'
@@ -48,16 +45,30 @@ export class ChatService {
   }
 
   getUsuarioId() {
-    if(this.usuarioObservable.value ===null){
+    if (this.usuarioObservable.value === null) {
       return Number(sessionStorage.getItem('usuario'));
     }
     return this.usuarioObservable.value;
   }
-  cargarMensajesChat(idPerfil: number | null): Observable<Mensaje[]> {
-    return this.http.get<any>(`${this.apiUrl+this.urlMensajesChatPrivado}`+idPerfil);
-  }
-  enviarMensaje(mensaje:Mensaje){
-    return this.http.post<any>(`${this.apiUrl+this.urlMensajeEnviar}`,mensaje );
+
+  cargarMensajesChat(chatId: number): Observable<Mensaje[]> {
+    console.log('Cargando mensajes del chat:', chatId);
+    sessionStorage.setItem('chat', String(chatId));
+    return this.http.get<any>(`${this.apiUrl + this.urlMensajesChatPrivado}` + chatId);
   }
 
+  enviarMensaje(mensaje: Mensaje) {
+    return this.http.post<any>(`${this.apiUrl + this.urlMensajeEnviar}`, mensaje);
+  }
+
+  setChatId(chatId: number) {
+    sessionStorage.setItem('chat', String(chatId));
+    this.usuarioObservable.next(chatId);
+  }
+  getChatId(): number  {
+    if(this.chatObservable.value ===null){
+      return Number(sessionStorage.getItem('contacto'));
+    }
+    return this.chatObservable.value;
+  }
 }
--- src/app/servicio/usuario.service.ts
+++ src/app/servicio/usuario.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 import {environment} from "../../environments/environment";
+import {Mensaje} from "../models/Mensaje";
+import {Perfil} from "../models/Perfil";
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,29 @@ import {environment} from "../../environments/environment";
 export class UsuarioService {
 
   private apiUrl = environment.apiUrl;
-  private perfilUrl = '/perfil/id/';
+  private perfilUrl = '/perfil/';
 
 
   constructor(private http: HttpClient) { }
+
+
+  private getHeaders(): HttpHeaders {
+    const token = sessionStorage.getItem('auth-token');
+    if (!token) {
+      console.error('❌ No hay token en sessionStorage. Asegúrate de que el usuario haya iniciado sesión.');
+      return new HttpHeaders();
+    }
+    console.log('✅ Token enviado en el header:', token);
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  }
+  // Método para obtener el perfil de un usuario
+  getNombrePerfil(): Observable<Perfil> {
+    return this.http.get<Perfil>(`/api/perfil/perfilLoggeado`, { headers: this.getHeaders() });
+  }
+
 
 
 }
